f3aa9296fb27fd74bed4404d6d6f9e38de8b1a9a
sanget <zh.sanget@gmail.com>
2019-10-21 08:44:44 +0800

    Update ui

--- ui-doc/src/components/GlobalStyle.tsx
+++ ui-doc/src/components/GlobalStyle.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { createGlobalStyle } from '@xstyled/styled-components';
-import { th } from '@xstyled/system';
-import { Normalize } from '@smooth-ui/core-sc';
+// import { createGlobalStyle } from '@xstyled/styled-components';
+// import { th } from '@xstyled/system';
+// import { Normalize } from '@smooth-ui/core-sc';
 
-const LocalGlobalStyle = createGlobalStyle`
-  html, body {
-    margin: 0;
-    // color: ${th.color('text')};
-    line-height: 1.4;
-  }
+import 'ukelli-ui/default.css';
 
-  a, a:hover {
-    color: ${th.color('primary')};
-    text-decoration: none;
-  }
-`;
+// const LocalGlobalStyle = createGlobalStyle`
+//   html, body {
+//     margin: 0;
+//     // color: ${th.color('text')};
+//     line-height: 1.4;
+//   }
+
+//   a, a:hover {
+//     color: ${th.color('primary')};
+//     text-decoration: none;
+//   }
+// `;
 
 export function GlobalStyle() {
   return (
     <>
-      <Normalize />
-      <LocalGlobalStyle />
+      {/* <Normalize />
+      <LocalGlobalStyle /> */}
     </>
   );
 }
