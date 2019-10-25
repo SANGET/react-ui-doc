abf638257716525831d539698c646b92705fb567
sanget <zh.sanget@gmail.com>
2019-10-25 09:10:30 +0800

    Update side nav

--- ui-doc/src/layouts/docs.tsx
+++ ui-doc/src/layouts/docs.tsx
@@ -1,6 +1,65 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import { graphql } from 'gatsby';
+import styled, { css } from 'styled-components';
 import DefaultLayout from './default';
+import { SideNav } from '../components/side-nav';
+import { Article } from '../components/archive';
+
+const MenuContext = React.createContext();
+
+const MenuProvider = ({ children }) => {
+  const [opened, setOpened] = useState(false);
+  const toggle = useCallback(() => setOpened((_opened) => !_opened), []);
+  const value = useMemo(() => ({ opened, toggle }), [opened, toggle]);
+  return <MenuContext.Provider value={value}>{children}</MenuContext.Provider>;
+};
+
+const Wrapper = styled.div`
+  width: 70%;
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+`;
+
+const SideNavWrapper = styled.div`
+  flex-basis: 200px;
+  
+  ${c => c.opened && css`
+    > svg:first-child {
+      transform: translate(-1px, 10px);
+    }
+
+    > svg:last-child {
+      transform: translate(-1px, -10px) rotate(180deg);
+    }
+  `}
+`;
+
+export default class DocLayout extends React.Component {
+  render() {
+    const { children, ...props } = this.props;
+    return (
+      <MenuProvider>
+        <DefaultLayout {...props}>
+          <Wrapper>
+            <MenuContext.Consumer>
+              {
+                ({ opened, toggle }) => {
+                  return (
+                    <SideNavWrapper opened={opened}>
+                      <SideNav />
+                    </SideNavWrapper>
+                  );
+                }
+              }
+            </MenuContext.Consumer>
+            <Article>{children}</Article>
+          </Wrapper>
+        </DefaultLayout>
+      </MenuProvider>
+    );
+  }
+}
 
 export const pageQuery = graphql`
   query BlogPostQueryDoc($id: String) {
@@ -13,14 +72,3 @@ export const pageQuery = graphql`
     }
   }
 `;
-
-export default class DocLayout extends React.Component {
-  render() {
-    const { children, ...props } = this.props;
-    return (
-      <DefaultLayout {...props}>
-        <div>{children}</div>
-      </DefaultLayout>
-    );
-  }
-}
