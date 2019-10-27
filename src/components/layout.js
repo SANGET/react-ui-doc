686f77884b7196c73245029b555f6f55be0a11c5
sanget <zh.sanget@gmail.com>
2019-10-27 17:09:05 +0800

    Update pages

---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { LiveConfig } from '@ukelli-ui/doc/components';
+import styled, * as sc from 'styled-components';
+import * as ukelliCore from 'ukelli-ui/core';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <LiveConfig
+        modules={{
+          react: React,
+          'ukelli-ui/core': ukelliCore,
+          'styled-components': Object.assign(styled, sc),
+        }}
+      />
+      {children}
+    </>
+  );
+}
