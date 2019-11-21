6b0c9f1d719f1ec3aebe6b62610ea5b9915c08be
sanget <zh.sanget@gmail.com>
2019-11-21 23:01:13 +0800

    Add page

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { LiveConfig } from '@deer-ui/doc-generator/components';
 import styled, * as sc from 'styled-components';
 import * as UICore from '@deer-ui/core';
+import * as UIUtils from '@deer-ui/core/utils';
 
 export default function Layout({ children }) {
   return (
@@ -10,6 +11,7 @@ export default function Layout({ children }) {
         modules={{
           react: React,
           '@deer-ui/core': UICore,
+          '@deer-ui/core/utils': UIUtils,
           'styled-components': Object.assign(styled, sc),
         }}
       />
