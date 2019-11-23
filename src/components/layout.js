45e5a58c8af26e7a1f7dcf1f0dd6b80030f72faa
sanget <zh.sanget@gmail.com>
2019-11-23 23:43:14 +0800

    Add UI doc pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { LiveConfig } from '@deer-ui/doc-generator/components';
 import styled, * as sc from 'styled-components';
 import * as UICore from '@deer-ui/core';
+import * as EnhanceUI from '@deer-ui/enhance-ui';
 import * as UIUtils from '@deer-ui/core/utils';
+import * as BaseFunc from '@mini-code/base-func';
+import * as MockDataForTable from './mock-data/table';
+import MockData from './mock-data/mock-data';
+import FormOptions from './mock-data/form-options';
 
 export default function Layout({ children }) {
   return (
     <>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/themes/airbnb.css" />
       <LiveConfig
         modules={{
           react: React,
           '@deer-ui/core': UICore,
+          '@deer-ui/enhance-ui': EnhanceUI,
           '@deer-ui/core/utils': UIUtils,
+          'mockdata-for-table': MockDataForTable,
+          '@mini-code/base-func': BaseFunc,
+          mockdata: MockData,
+          formOptions: FormOptions,
           'styled-components': Object.assign(styled, sc),
         }}
       />
