eba0f01c3e44a6caa0be8ff13fc14cb7f5bd07a0
Alex <zh.sanget@gmail.com>
2019-11-29 12:00:30 +0800

    v0.2.0-alpha1.7

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -1,45 +1,150 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
-import { Grid } from '@deer-ui/core';
+import { Grid, Icon } from '@deer-ui/core';
+import Showcase from './showcase';
+import { Box } from './common';
 
 const Container = styled.div`
-  width: 980px;
+  width: 100%;
+  max-width: 1200px;
   margin: 40px auto 0;
 `;
 
+const Wrapper = styled.div`
+  /* background-color: #fafafa; */
+  padding: 0.1px;
+  font-size: 1.125rem;
+  padding-bottom: 40px;
+`;
+
 const UIName = styled.div`
-  font-size: 40px;
+  font-size: 30px;
   margin-bottom: 20px;
 `;
 
-const UILogo = styled.div`
-  font-size: 40px;
+const UILogo = styled(Grid)`
+  /* font-size: 40px; */
+  background-color: #376BFB;
+  border-radius: 50%;
+  width: 120px !important;
+  padding: 16px;
+  transition: all 0.5s ease;
+
+  img {
+    width: 100%;
+  }
+
+  &:hover {
+    transform: translateY(-10px);
+  }
 `;
 
 const Desc = styled.div`
   font-size: 18px;
-  margin-bottom: 20px;
+  width: 100%;
+  max-width: 500px;
+  margin: 40px auto;
+  line-height: 28px;
+`;
+
+const MainIntro = styled.div`
+  text-align: center;
+  margin-bottom: 50px;
+`;
+
+const FeatherIntro = styled.div`
+  margin-bottom: 80px;
+`;
+
+const FeatherIntroItem = styled(Box)`
+  height: 160px;
+  padding: 10px;
 `;
 
 const HomePage = () => {
   return (
-    <Container>
-      <Grid container>
-        <Grid lg={4}>
-          <UILogo>@deer-ui</UILogo>
-        </Grid>
-        <Grid lg={8}>
-          <UIName>Deer UI</UIName>
+    <Wrapper>
+      <Container>
+        <MainIntro>
+          <UIName>
+          Building Form Group by the easiest way
+          </UIName>
           <Desc>
-            可扩展的 UI 库
+          Deer ui is a Extendable's UI lib base on React
+          , provide FormGenerator to help us to build form logic clearly
+          , and also provide a useful Table to display data
           </Desc>
           <Link
             className="btn theme hola"
-            to="/docs/getting-started/">Getting Started</Link>
+            to="/docs/">
+          Getting Started
+            <Icon n="angle-right" classNames={['ml10']} />
+          </Link>
+        </MainIntro>
+        <FeatherIntro>
+          <Grid container space={10}>
+            <Grid
+              lg={3}
+              sm={6}
+              xs={12}>
+              <FeatherIntroItem>
+                <h4>Typescript support</h4>
+                <div>
+                Base on typescript to construct
+                , totally support code hint and type check, safe to use.
+                </div>
+              </FeatherIntroItem>
+            </Grid>
+            <Grid
+              lg={3}
+              sm={6}
+              xs={12}>
+              <FeatherIntroItem>
+                <h4>Devices support</h4>
+                <div>
+                Take into account the different between Desktop and Mobile web app
+                , automate detect device and choice the right way to render.
+                </div>
+              </FeatherIntroItem>
+            </Grid>
+            <Grid
+              lg={3}
+              sm={6}
+              xs={12}>
+              <FeatherIntroItem>
+                <h4>SSR support</h4>
+                <div>
+                Support both client side add server side render
+                , like Gatsby and Next
+                </div>
+              </FeatherIntroItem>
+            </Grid>
+            <Grid
+              lg={3}
+              sm={6}
+              xs={12}>
+              <FeatherIntroItem>
+                <h4>Extendable</h4>
+                <div>
+                Provide the extendable way to organiz each Component's relationship
+                , like @deer-ui/core, @deer-ui/enhance-ui and @deer-ui/admin-scaffold
+                </div>
+              </FeatherIntroItem>
+            </Grid>
+          </Grid>
+        </FeatherIntro>
+        <Grid
+          container
+          direction="col"
+          alignContent="center">
+          <UILogo container justify="center" alignItems="center">
+            <img src={require('../images/logo.png')} alt=""/>
+          </UILogo>
         </Grid>
-      </Grid>
-    </Container>
+        <Showcase />
+      </Container>
+    </Wrapper>
   );
 };
 
